54293e482bb4980e24f7a444b6009cbf82aeb9c4
Musa Rafik <musarafik@Musas-MacBook-Pro.local>
2024-12-17 22:43:02 -0500

    Initial Commit

---
+++ Cook Book/Cook_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cook_BookApp.swift
+//  Cook Book
+//
+//  Created by Musa Rafik on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Cook_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
